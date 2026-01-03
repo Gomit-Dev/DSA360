a36ad5fb5dbeece386c194df6eba3ce46c9e3af2
Gomit Saha <gomit.dev@gmail.com>
2026-01-03 22:47:37 +0530

    Add BFS and DFS methods to Graph class
    Implemented BFS and DFS traversals for a graph class.

--- Day3.cpp
+++ Day3.cpp
@@ -1,1 +1,81 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+    int V;                      // number of vertices
+    vector<vector<int>> adj;   // adjacency list
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adj.resize(V);
+    }
+
+    // add edge
+    void addEdge(int u, int v, bool directed = false) {
+        adj[u].push_back(v);
+        if (!directed)
+            adj[v].push_back(u);
+    }
+
+    // BFS traversal
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        cout << "BFS: ";
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            cout << node << " ";
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    // DFS helper
+    void DFSUtil(int node, vector<bool>& visited) {
+        visited[node] = true;
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    // DFS traversal
+    void DFS(int start) {
+        vector<bool> visited(V, false);
+        cout << "DFS: ";
+        DFSUtil(start, visited);
+        cout << endl;
+    }
+};
+
+int main() {
+    Graph g(6);
+
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(3, 5);
+    g.addEdge(4, 5);
+
+    g.BFS(0);
+    g.DFS(0);
+
+    return 0;
+}
